1ce1bda59a03ede22b117267419f7bff0d137e3d
Dominik Lukasiewicz <dominikluk2@gmail.com>
2025-10-10 14:36:29 +0200

    Create NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from "vue";
 import { useCounterStore } from "./stores/counter";
+import NavBar from "./components/NavBar.vue";
 
 const counter = useCounterStore();
 </script>
 
 <template>
+  <NavBar></NavBar>
   {{ counter.count }}
   <button @click="counter.increment()">Count</button>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const links = ['Home', 'Algorithms', 'Visualization']
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <a class="navbar-brand" href="#">Sorting Algorithms</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li v-for="link in links" class="nav-item">
+          <a class="nav-link" href="#">{{ link }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
