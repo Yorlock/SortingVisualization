d0503eef6ae60afb9de28ec99506f31d4161811d
Dominik Lukasiewicz <dominikluk2@gmail.com>
2025-10-19 15:19:17 +0200

    use local storage

--- src/stores/navBar.ts
+++ src/stores/navBar.ts
@@ -2,8 +2,8 @@ import { defineStore } from "pinia";
 
 export const useNavBarStore = defineStore("navBar", {
   state: () => ({
-    activePage: 0,
-    isDarkNavbar: false,
+    activePage: JSON.parse(localStorage.getItem('activePage') ?? '0'),
+    isDarkNavbar: JSON.parse(localStorage.getItem('isDarkNavbar') ?? 'false'),
   }),
   getters: {
     getNavBarStyle: (state) =>
@@ -14,9 +14,11 @@ export const useNavBarStore = defineStore("navBar", {
   actions: {
     changePage(newPage: number) {
       this.activePage = newPage;
+      localStorage.setItem('activePage', JSON.stringify(this.activePage))
     },
     changeNavbarMode() {
       this.isDarkNavbar = !this.isDarkNavbar;
+      localStorage.setItem('isDarkNavbar', JSON.stringify(this.isDarkNavbar))
     },
   },
 });
